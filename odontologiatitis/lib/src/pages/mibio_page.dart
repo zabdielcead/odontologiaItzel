f4a50c7b9ed601de5e528f498ae3bde8678e7f5f
zabdielcead <zabdiel.cead@gmail.com>
2020-11-27 20:13:32 -0600

    cambio appbar

--- odontologiatitis/lib/src/pages/mibio_page.dart
+++ odontologiatitis/lib/src/pages/mibio_page.dart
@@ -6,18 +6,21 @@ class MiBioPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          appBar: _createAppBar(),
-          body: Stack(
-            children: <Widget>[              
-                
-                     _crearFondo(context),
-                     
+    return SafeArea(
+          top: true,
+          child: Scaffold(
+            appBar: _createAppBar(),
+            body: Stack(
+              children: <Widget>[              
                   
-              
-            ],
-          )
-        );
+                       _crearFondo(context),
+                       
+                    
+                
+              ],
+            )
+          ),
+    );
   }
 
 
